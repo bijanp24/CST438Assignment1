ec5260c059a3908dd7e5403bb0d3dede711564fb
bijanlee <57022377+bijanlee@users.noreply.github.com>
2021-05-03 18:30:57 -0700

    assignment 1 finished

---
+++ cst438Assignment1/src/main/java/cst438/MovieRatingRepository.java
@@ -0,0 +1,12 @@
+package cst438;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MovieRatingRepository extends CrudRepository<MovieRating, Long>
+{
+   @Query("select m from MovieRating m order by title, dateTime desc")
+   List<MovieRating> findAllMovieRatingsOrderByTitleDateDesc();
+}
